049b60266cc11cccbe7f2bd15ddc71e9cb42078d
guanshaoheng <guanshaoheng>
2021-11-05 18:04:32 +0000

    first commit

--- Gaussian.py
+++ Gaussian.py
@@ -1,14 +1,20 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 
 class GuanssianRandomPath:
-    def __init__(self, curlDegree, amplitudeValue, seed=10001, showFlag=False):
-        self.seed = seed
+    """
+        Used for random loading path generation via Gaussian Process method
+    """
+    def __init__(self, curlDegree, amplitudeValue, showFlag=False, generatingNum=0, maxEpsilonLimitation=0.8):
+        self.seed = 10001
         np.random.seed(self.seed)
 
         self.showFlag = showFlag
+        self.generatingNum = generatingNum
+        self.maxEpsilonLimitation = maxEpsilonLimitation
         self.numberOfPoints = 1000
         self.numberOfFuncutions = 3
 
@@ -25,7 +31,25 @@ class GuanssianRandomPath:
         if self.showFlag:
             self.plotPaths()
             self.plotCovarianceMatrix(kernel=self.cov, curl=self.curlDegree)
-        self.writeDownPaths()
+
+        if self.generatingNum>0:
+            self.generation()
+
+    def generation(self):
+        i = 0
+        while i < self.generatingNum:
+            self.seed = i
+            np.random.seed(self.seed)
+            self.y = np.random.multivariate_normal(mean=np.ones(self.numberOfPoints)*self.meanValue,
+                                               cov=self.cov,
+                                               size=self.numberOfFuncutions)
+            maxEpsilon = np.max(np.abs(self.y))
+            if maxEpsilon > self.maxEpsilonLimitation:
+                continue
+            else:
+                self.plotPaths(path='MCCData')
+                self.writeDownPaths()
+                i += 1
 
     def CovarienceMatrix(self, x, y):
         """
@@ -47,7 +71,7 @@ class GuanssianRandomPath:
         plt.tight_layout()
         plt.savefig('./figSav/CovariabceHeatMap_curl%d.png' % curl, dpi=200)
 
-    def plotPaths(self):
+    def plotPaths(self, path='figSav'):
         # Plot the sampled functions
         fig, ax = plt.subplots(1, 1, figsize=(6, 4), dpi=100)
         totalPointsOnFigure = 50
@@ -59,22 +83,19 @@ class GuanssianRandomPath:
         plt.xlabel('Loading step', fontsize=15)
         plt.ylabel('$\epsilon$', fontsize=15)
         plt.xticks(fontsize=15)
-        # plt.title((
-        #         '%d different function realizations at %d points sampled from\n' % (number_of_functions, nb_of_samples) +
-        #         ' a Gaussian process with exponentiated quadratic kernel'))
         plt.xlim([0, len(self.y[0])])
         plt.tight_layout()
         plt.legend()
-        plt.savefig('./figSav/ConfiningPressureGP_curl%d.png' % self.curlDegree, dpi=200)
+        figName = 'ConfiningPressureGP_curl%d_seed%d.png' % (self.curlDegree, self.seed)
+        plt.savefig(os.path.join(path, figName), dpi=200)
+        plt.close()
 
     def writeDownPaths(self):
         filePath = './MCCData/path_%d.dat' % self.seed
         np.savetxt(fname=filePath, X=self.y.T, fmt='%10.5f', delimiter=',', header='epsilon_xx, epsilon_yy, epsilon_xy')
 
 
-# gaussian = GuanssianRandomPath(curlDegree=5, amplitudeValue=0.25)  # generally 1~5, 0.25
+gaussian = GuanssianRandomPath(curlDegree=2, amplitudeValue=0.15, generatingNum=10)  # generally 1~5, 0.25
 
-for i in range(100):
-    gaussian = GuanssianRandomPath(curlDegree=2, amplitudeValue=0.25)
 
 
