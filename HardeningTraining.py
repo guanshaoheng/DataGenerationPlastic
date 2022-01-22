aa0d80df39f72a7ab679ecfc1a0650205689fbb0
guanshaoheng <191359602@qq.com>
2022-01-22 15:47:56 +0000

    mises model training via pytorch

--- HardeningTraining.py
+++ HardeningTraining.py
@@ -5,34 +5,59 @@ from misesTraining import Net, modelTrainning, Restore, findDevice, pickle_dump,
 import numpy as np
 import torch
 import time
+from misesTraining import SSP
+from torch import nn
 
 
 if __name__ == "__main__":
-    if not os.path.exists('HardeningModel'):
-        os.mkdir('HardeningModel')
-    layerList = 'dmmdmd'
-    savedPath = os.path.join('HardeningModel', layerList)
+    layerList = 'mmmmd'
+    residualFlag = False
+    savedPath = os.path.join('HardeningModel', 'ResidualNet' if residualFlag else layerList)
+    functionGeneration = False
+    normalizationFlag = False
+    fourier_features = True
+    savedPath += ('_Fourier' if fourier_features else '_noFourier')
+    savedPath += ('_generation' if functionGeneration else '_data')
+    savedPath += ('_scaled' if normalizationFlag else '_Noscaled')
+    print()
+    print('-' * 80)
+    print(savedPath)
     if not os.path.exists(savedPath):
         os.mkdir(savedPath)
+    if functionGeneration:
+        epsPlas, H, dHdEps = dataReader(root_path=savedPath, readContent='epsPlastic')
+    else:
+        epsPlas, H, dHdEps = dataReader(root_path=savedPath, readContent='epsPlastic')
 
-    device = findDevice()
-
-    epsPlas, H, dHdEps = dataReader(root_path=savedPath, readContent='epsPlastic')
-
-
-    sampleNum = 1000
+    sampleNum = 5000
     index_random = np.random.permutation(range(len(epsPlas)))
-    epsPlas, H, dHdEps = epsPlas[index_random[:sampleNum]], \
+    x, y, dy = epsPlas[index_random[:sampleNum]], \
                              H[index_random[:sampleNum]], dHdEps[index_random[:sampleNum]]
 
     # ------------------------------------------------
+    device = findDevice()
     # model training
-    misesNet = Net(inputNum=1, outputNum=1, layerList=layerList).to(device=device)
-    mTrain = modelTrainning(misesNet, savedPath=savedPath, device=device, normalizationFlag=True)
-    mTrain.trainModel(x=epsPlas, y=H, dy=dHdEps)
+    x_min, x_max, y_min, y_max, dy_min, dy_max, y_std, dy_std = \
+        pickle_load('x_min', 'x_max', 'y_min', 'y_max', 'dy_min', 'dy_max', 'y_std', 'dy_std', root_path=savedPath)
+    if residualFlag:
+        # misesNet = DenseResNet(xmin=x_min, xmax=x_max, ymin=y_min, ymax=y_max, device=device,
+        #                    inputNum=3, outputNum=1, fourier_features=fourier_features).to(device=device)
+        misesNet = Net(xmin=x_min, xmax=x_max, ymin=y_min, ymax=y_max, device=device,
+                       activation=nn.Sigmoid(), inputNum=1, outputNum=1, layerList=layerList, node=100,
+                       fourier_features=fourier_features, m_freqs=50).to(device=device)
+    else:
+        misesNet = Net(xmin=x_min, xmax=x_max, ymin=y_min, ymax=y_max, device=device,
+                       activation=nn.Sigmoid(), inputNum=1, outputNum=1, layerList=layerList, node=100,
+                       fourier_features=fourier_features, m_freqs=50).to(device=device)
+    mTrain = modelTrainning(model=misesNet, savedPath=savedPath,
+                            device=device, optimizerSTR='adam',
+                            normalizationFlag=normalizationFlag,
+                            epochMax=int(1e6), batchSize=0, dyWeight=10. * (y_std[0] / dy_std[0]) ** 2, patienceNum=100)
+    mTrain.trainModel(x=x, y=y, dy=dy)
 
     # restore
-    model_evaluation = Restore(savedPath=savedPath, device=device, normalizationFlag=True)
-    model_evaluation.evaluation(x=epsPlas, y=H, dy=dHdEps)
+    # model_evaluation = Restore(savedPath=os.path.join(savedPath, 'epoch_3000'),
+    #                            device=device, normalizationFlag=normalizationFlag)
+    model_evaluation = Restore(savedPath=savedPath, device=device, normalizationFlag=normalizationFlag)
+    model_evaluation.evaluation(x=x, y=y, dy=dy)
     print()
-
--- misesTraining.py
+++ misesTraining.py
@@ -20,7 +20,8 @@ class SSP(nn.Softplus):
 
 class Net(nn.Module):
     def __init__(self, xmin, xmax, ymin, ymax, device,
-                 activation=SSP(), inputNum=4, outputNum=1, layerList='dddmd', node=100):
+                 activation=nn.Sigmoid(), inputNum=4, outputNum=1, layerList='dmddmd', node=100,
+                 fourier_features=True, m_freqs=100, sigma=10.):
         super(Net, self).__init__()
         self.inputNum = inputNum
         self.outputNum = outputNum
@@ -28,10 +29,18 @@ class Net(nn.Module):
         self.layerList = layerList
         self.layers = torch.nn.ModuleList(self.getInitLayers())
         self.activation = activation
+        self.fourier_features = fourier_features
+        self.m_freqs = m_freqs
+        self.device = device
         self.xmin, self.xmax, self.ymin, self.ymax = torch.tensor(xmin, dtype=torch.float, device=device), \
                                                      torch.tensor(xmax, dtype=torch.float, device=device), \
                                                      torch.tensor(ymin, dtype=torch.float, device=device), \
                                                      torch.tensor(ymax, dtype=torch.float, device=device)
+        if fourier_features:
+            self.first = nn.Linear(2*m_freqs, node)
+            self.B = nn.Parameter(sigma*torch.randn(inputNum, m_freqs)).to(self.device) # to converts inputs to m_freqs
+        else:
+            self.first = nn.Linear(inputNum, node)
 
     def normalization(self, x, xmin, xmax, reverse=False):
         if reverse:
@@ -45,7 +54,7 @@ class Net(nn.Module):
         num_layers = len(self.layerList)
         for i in range(num_layers):
             if i == 0:
-                layers.append(nn.Linear(self.inputNum, self.node))
+                layers.append(nn.Linear(self.node, self.node))
             elif i == num_layers - 1:
                 layers.append(nn.Linear(self.node, self.outputNum))
             else:
@@ -55,51 +64,134 @@ class Net(nn.Module):
     def forward(self, x):
         # normalization
         x = self.normalization(x, self.xmin, self.xmax)
+
+        if self.fourier_features:
+            cosx = torch.cos(torch.matmul(x, self.B))
+            sinx = torch.sin(torch.matmul(x, self.B))
+            x = torch.cat((cosx, sinx), dim=1)
+            x = self.activation(self.first(x))
+
+        else:
+            x = self.activation(self.first(x))
+
         num_layers = len(self.layers)
-        for i, key in enumerate(self.layerList[:-1]):
+        i = 0
+        while i < (num_layers-1):
+            key = self.layerList[i]
             if key == 'd':
-                x = self.activation(self.layers[i](x))
+                x = torch.relu(self.layers[i](x))
+                i += 1
+            elif key == 'm':
+                x = torch.relu(self.layers[i](x * x))+x
+                i += 1
             else:
-                x = self.activation(self.layers[i](x * x))
+                raise ValueError('Key cannot be %s' % key)
         x = self.layers[num_layers - 1](x)
         # reverse normalization
-        y = self.normalization(x, self.ymin, self.ymax, reverse=False)
+        y = self.normalization(x, self.ymin, self.ymax, reverse=True)
         return y
 
 
-class NetContrained(nn.Module):
-    def __init__(self, activation=nn.ReLU(), inputNum=3, outputNum=1, layerList='dddd', node=100):
-        super(NetContrained, self).__init__()
-        self.inputNum = inputNum
-        self.outputNum = outputNum
-        self.node = node
+class DenseResNet(nn.Module):
+    """
+    This is a ResNet Class.
+    -> dim_in: network's input dimension
+    -> dim_out: network's output dimension
+    -> num_resnet_blocks: number of ResNet blocks
+    -> num_layers_per_block: number of layers per ResNet block
+    -> num_neurons: number of neurons in each layer
+    -> activation: Non-linear activations function that you want to use. E.g. nn.Sigmoid(), nn.ReLU()
+    -> fourier_features: whether to pass the inputs through Fourier mapping. E.g. True or False
+    -> m_freq: how many frequencies do you want the inputs to be mapped to
+    -> sigma: controls the spectrum of frequencies.
+              If sigma is greater more frequencies are consider.
+              You can also look at it as sampling from the standard normal, Z~N(0, 1),
+              and mapping to another normal, X~N(\mu, \sigma^2), using x = mu + sigma*z.
+    -> tune_beta: do you want to consider the parameter beta in the activation functions in each layer? E.g., Tanh(beta*x).
+                  In practice it is observed that training beta (i.e. tune_beta=True) could improve convergence.
+                  If tune_beta=False, you get the a fixed beta i.e. beta=1.
+    -> The method model_capacity() returns the number of layers and parameters in the network.
+    """
+    def __init__(self, xmin, xmax, ymin, ymax, device,
+                 inputNum=4, outputNum=1, num_resnet_blocks=3,
+                 num_layers_per_block=2, num_neurons=50, activation=nn.Sigmoid(),
+                 fourier_features=True, m_freqs=100, sigma=10, tune_beta=False):
+        super(DenseResNet, self).__init__()
+        self.device = device
+        self.num_resnet_blocks = num_resnet_blocks
+        self.num_layers_per_block = num_layers_per_block
+        self.fourier_features = fourier_features
         self.activation = activation
-        self.layerList = layerList
-        self.layers = torch.nn.ModuleList(self.getInitLayers())
+        self.tune_beta = tune_beta
+        self.xmin, self.xmax, self.ymin, self.ymax = torch.tensor(xmin, dtype=torch.float, device=device), \
+                                                     torch.tensor(xmax, dtype=torch.float, device=device), \
+                                                     torch.tensor(ymin, dtype=torch.float, device=device), \
+                                                     torch.tensor(ymax, dtype=torch.float, device=device)
 
-    def getInitLayers(self, ):
-        layers = []
-        num_layers = len(self.layerList)
-        for i in range(num_layers):
-            if i == 0:
-                layers.append(nn.Linear(9, self.node))
-            elif i == num_layers - 1:
-                layers.append(nn.Linear(self.node, self.outputNum))
-            else:
-                layers.append(nn.Linear(self.node, self.node))
-        return layers
+        if tune_beta:
+            self.beta0 = nn.Parameter(torch.ones(1, 1)).to(self.device)
+            self.beta = nn.Parameter(torch.ones(self.num_resnet_blocks, self.num_layers_per_block)).to(self.device)
+
+        else:
+            self.beta0 = torch.ones(1, 1).to(self.device)
+            self.beta = torch.ones(self.num_resnet_blocks, self.num_layers_per_block).to(self.device)
+
+        self.first = nn.Linear(inputNum, num_neurons).to(self.device)
+
+        self.resblocks = nn.ModuleList([
+            nn.ModuleList([nn.Linear(num_neurons, num_neurons)
+                for _ in range(num_layers_per_block)])
+            for _ in range(num_resnet_blocks)])
+
+        self.last = nn.Linear(num_neurons, outputNum)
+
+        if fourier_features:
+            self.first = nn.Linear(2*m_freqs, num_neurons)
+            self.B = nn.Parameter(sigma*torch.randn(inputNum, m_freqs)).to(self.device) # to converts inputs to m_freqs
 
     def forward(self, x):
-        x2 = x*x
-        x = torch.concat((x, x2, x[:, 0:1]*x[:, 1:2], x[:, 1:2]*x[:, 2:3], x[:, 0:1]*x[:, 2:3],), axis=1)
-        num_layers = len(self.layers)
-        for i, key in enumerate(self.layerList[:-1]):
-            if key == 'd':
-                x = self.activation(self.layers[i](x))
-            else:
-                x = self.activation(self.layers[i](x * x))
-        x = self.layers[num_layers - 1](x)
-        return x
+        # normalization
+        x = self.normalization(x, self.xmin, self.xmax)
+
+        if self.fourier_features:
+            cosx = torch.cos(torch.matmul(x, self.B))
+            sinx = torch.sin(torch.matmul(x, self.B))
+            x = torch.cat((cosx, sinx), dim=1)
+            x = self.activation(self.beta0*self.first(x))
+
+        else:
+            x = self.activation(self.beta0*self.first(x))
+
+        for i in range(self.num_resnet_blocks):
+            z = self.activation(self.beta[i][0]*self.resblocks[i][0](x))
+
+            for j in range(1, self.num_layers_per_block):
+                z = self.activation(self.beta[i][j]*self.resblocks[i][j](z))
+
+            x = z + x
+
+        out = self.last(x)
+
+        # Reverse normalization
+        out = self.normalization(out, self.ymin, self.ymax, reverse=True)
+        return out
+
+    def normalization(self, x, xmin, xmax, reverse=False):
+        if reverse:
+            normed = (xmax-xmin)*x+xmin
+        else:
+            normed = (x-xmin)/(xmax-xmin)
+        return normed
+
+    def model_capacity(self):
+        """
+        Prints the number of parameters and the number of layers in the network
+        """
+        number_of_learnable_params = sum(p.numel() for p in self.parameters() if p.requires_grad)
+        num_layers = len(list(self.parameters()))
+        print("\n\nThe number of layers in the model: %d" % num_layers)
+        print("\nThe number of learnable parameters in the model: %d" % number_of_learnable_params)
+
 
 def getH(epsPlastic):
     H = 500e6 * (0.05 + epsPlastic) ** 0.2
@@ -107,7 +199,7 @@ def getH(epsPlastic):
 
 
 def get_dHdEps(epsPlastic):
-    dHdEps = -500e6 * 0.2 * (0.05 + epsPlastic) ** (0.2 - 1.)
+    dHdEps = 500e6 * 0.2 * (0.05 + epsPlastic) ** (0.2 - 1.)
     return dHdEps
 
 
@@ -249,8 +341,8 @@ class modelTrainning:
         self.state = copy.deepcopy(self.model.state_dict())
         self.num_batches = 10
         if 'adam' in optimizerSTR:
-            # self.optimizer = torch.optim.Adam(self.model.parameters(), )
-            self.optimizer = torch.optim.NAdam(self.model.parameters(), )
+            self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.002)
+            # self.optimizer = torch.optim.NAdam(self.model.parameters(), )
             self.verboseInterval = 10
         elif 'lbfgs' in optimizerSTR:
             self.optimizer = torch.optim.LBFGS(
@@ -259,11 +351,11 @@ class modelTrainning:
         else:
             raise ValueError('Please input a right keyword for optimizer selection! (%s) ' % optimizerSTR)
 
-        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=2000, gamma=0.95)
-        # self.x_mean, self.x_std, self.y_mean, self.y_std, self.dy_mean, self.dy_std, \
-        # self.x_min, self.x_max, self.y_min, self.y_max, self.dy_min, self.dy_max = \
-        #     pickle_load('x_mean', 'x_std', 'y_mean', 'y_std', 'dy_mean', 'dy_std',
-        #                 'x_min', 'x_max', 'y_min', 'y_max', 'dy_min', 'dy_max', root_path=self.savedPath)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=800, gamma=0.9)
+        self.x_mean, self.x_std, self.y_mean, self.y_std, self.dy_mean, self.dy_std, \
+        self.x_min, self.x_max, self.y_min, self.y_max, self.dy_min, self.dy_max = \
+            pickle_load('x_mean', 'x_std', 'y_mean', 'y_std', 'dy_mean', 'dy_std',
+                        'x_min', 'x_max', 'y_min', 'y_max', 'dy_min', 'dy_max', root_path=self.savedPath)
         # self.x_min = torch.tensor(self.x_min, dtype=torch.float, device=self.device)
         # self.x_max = torch.tensor(self.x_max, dtype=torch.float, device=self.device)
         # self.y_min = torch.tensor(self.y_min, dtype=torch.float, device=self.device)
@@ -320,7 +412,8 @@ class modelTrainning:
                 loss1 += loss1_batch*len(batch)
 
             if epoch % self.verboseInterval == 0:
-                loss = loss0+self.dyWeight*loss1
+                loss = (loss0/np.mean(np.abs(self.y_std))**2+loss1/np.mean(np.abs(self.dy_std))**2)/len(x_normed)
+                loss0, loss1 = loss0/len(x_normed), loss1/len(x_normed)
                 if loss < minLoss:
                     trialNum = 0
                     minLoss = loss
@@ -330,8 +423,8 @@ class modelTrainning:
                     trialNum += 1
                     message = 'Noimproved! in %d tirals' % (trialNum * self.verboseInterval)
 
-                info = "Epoch: %d \t lr: %.3e \t Loss: %.3e \t Loss0: %.3e \t Loss1: %.3e \t %s \ttimeConsumed: %.3e mins\n" % \
-                       (epoch, self.optimizer.param_groups[0]['lr'], loss, loss0, loss1, message, (time.time() - startTime) / 60.)
+                info = "Epoch: %d \t lr: %.3e \t Loss: %.3e/%.3e \t Loss0: %.3e \t Loss1: %.3e \t %s \ttimeConsumed: %.3e mins\n" % \
+                       (epoch, self.optimizer.param_groups[0]['lr'], loss, minLoss, loss0, loss1, message, (time.time() - startTime) / 60.)
                 print(info)
                 writeDown(info, self.savedPath, appendFlag=True)
 
@@ -351,7 +444,7 @@ class modelTrainning:
                     print(info)
                     writeDown(info, self.savedPath, appendFlag=True)
                     break
-            if (epoch+1) % (self.verboseInterval*500) == 0:
+            if (epoch+1) % (self.verboseInterval*100) == 0:
                 saveDir = os.path.join(self.savedPath, 'epoch_%d' % (epoch+1))
                 try:
                     os.mkdir(saveDir)
@@ -379,14 +472,14 @@ class modelTrainning:
     def trainOnce(self, x, y, dy):
         self.optimizer.zero_grad()
         yPrediction = self.model(x)
-        dyPrediction_dx = torch.autograd.grad(outputs=yPrediction,
+        dyPrediction = torch.autograd.grad(outputs=yPrediction,
                                               inputs=x,
                                               grad_outputs=torch.ones_like(yPrediction).to(self.device),
                                               retain_graph=True,
                                               create_graph=True)[0]
         # norm = torch.linalg.norm(dyPrediction_dx, dim=1)
         loss0 = self.lossCalculator(yPrediction, y)
-        loss1 = self.lossCalculator(dyPrediction_dx, dy)
+        loss1 = self.lossCalculator(dyPrediction, dy)
         loss = loss0+self.dyWeight*loss1
         loss.backward()
         self.optimizer.step()
@@ -398,11 +491,11 @@ class Restore:
         self.device = device
         self.savedPath = savedPath
         self.normalizationFlag = normalizationFlag
-        self.model = torch.load(os.path.join(savedPath, 'entire_model.pt')).to(device)
-        # self.x_mean, self.x_std, self.y_mean, self.y_std, self.dy_mean, self.dy_std, \
-        # self.x_min, self.x_max, self.y_min, self.y_max, self.dy_min, self.dy_max = \
-        #     pickle_load('x_mean', 'x_std', 'y_mean', 'y_std', 'dy_mean', 'dy_std',
-        #                 'x_min', 'x_max', 'y_min', 'y_max', 'dy_min', 'dy_max', root_path=self.savedPath)
+        mName = os.path.join(savedPath, 'entire_model.pt')
+        self.model = torch.load(mName).to(device)
+        print()
+        print('-'*80)
+        print('Model restored from %s' % mName)
 
     def evaluation(self, x, y, dy):
         y_origin, dy_origin = self.prediction(x)
@@ -412,16 +505,22 @@ class Restore:
         ax.scatter(y.reshape(-1), y_origin.reshape(-1))
         plt.tight_layout()
         plt.axis('equal')
-        plt.savefig(os.path.join(self.savedPath, 'Prediction.png'), dpi=200)
+        fname = os.path.join(self.savedPath, 'Prediction.png')
+        plt.savefig(fname, dpi=200)
         plt.close()
+        print()
+        print('fig saved as %s' % fname)
 
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ax.scatter(dy.reshape(-1), dy_origin.reshape(-1))
         plt.tight_layout()
         plt.axis('equal')
-        plt.savefig(os.path.join(self.savedPath, 'dPrediction.png'), dpi=200)
+        fname = os.path.join(self.savedPath, 'dPrediction.png')
+        plt.savefig(fname, dpi=200)
         plt.close()
+        print()
+        print('fig saved as %s' % fname)
 
     def prediction(self, x):
         if self.normalizationFlag:
@@ -506,11 +605,17 @@ def dataReader(root_path, filePath = './MCCData/results', readContent='sig'):
 
 if __name__ == "__main__":
     layerList = 'dmdmd'
-    savedPath = os.path.join('misesModel', layerList)
+    residualFlag = False
+    savedPath = os.path.join('misesModel', 'ResidualNet' if residualFlag else layerList)
     functionGeneration = False
     normalizationFlag = False
+    fourier_features = True
+    savedPath += ('_Fourier' if fourier_features else '_noFourier')
     savedPath += ('_generation' if functionGeneration else '_data')
     savedPath += ('_scaled' if normalizationFlag else '_Noscaled')
+    print()
+    print('-'*80)
+    print(savedPath)
     if not os.path.exists(savedPath):
         os.mkdir(savedPath)
     if functionGeneration:
@@ -525,20 +630,28 @@ if __name__ == "__main__":
                              mises[index_random[:sampleNum]], dmisesdsig[index_random[:sampleNum]]
 
     # ------------------------------------------------
-    # model training
     device = findDevice()
-    x_min, x_max, y_min, y_max, dy_min, dy_max = \
-        pickle_load('x_min', 'x_max', 'y_min', 'y_max', 'dy_min', 'dy_max', root_path=savedPath)
-    misesNet = Net(xmin=x_min, xmax=x_max, ymin=y_min, ymax=y_max, device=device,
-                   activation=SSP(), inputNum=3, outputNum=1, layerList=layerList, node=20).to(device=device)
+    # model training
+    x_min, x_max, y_min, y_max, dy_min, dy_max, y_std, dy_std = \
+        pickle_load('x_min', 'x_max', 'y_min', 'y_max', 'dy_min', 'dy_max', 'y_std', 'dy_std', root_path=savedPath)
+    if residualFlag:
+        # misesNet = DenseResNet(xmin=x_min, xmax=x_max, ymin=y_min, ymax=y_max, device=device,
+        #                    inputNum=3, outputNum=1, fourier_features=fourier_features).to(device=device)
+        misesNet = Net(xmin=x_min, xmax=x_max, ymin=y_min, ymax=y_max, device=device,
+                   activation=nn.Sigmoid(), inputNum=3, outputNum=1, layerList=layerList, node=100,
+                       fourier_features=fourier_features).to(device=device)
+    else:
+        misesNet = Net(xmin=x_min, xmax=x_max, ymin=y_min, ymax=y_max, device=device,
+                   activation=nn.Sigmoid(), inputNum=3, outputNum=1, layerList=layerList, node=100,
+                       fourier_features=fourier_features).to(device=device)
     mTrain = modelTrainning(model=misesNet, savedPath=savedPath,
                             device=device, optimizerSTR='adam',
                             normalizationFlag=normalizationFlag,
-                            epochMax=int(1e6), batchSize=0, dyWeight=4e8, patienceNum=50)
+                            epochMax=int(1e6), batchSize=0, dyWeight=10.*(y_std[0]/dy_std[0])**2, patienceNum=100)
     mTrain.trainModel(x=sig, y=mises, dy=dmisesdsig)
 
     # restore
-    # model_evaluation = Restore(savedPath=os.path.join(savedPath, 'epoch_149999'),
+    # model_evaluation = Restore(savedPath=os.path.join(savedPath, 'epoch_3000'),
     #                            device=device, normalizationFlag=normalizationFlag)
     model_evaluation = Restore(savedPath=savedPath, device=device, normalizationFlag=normalizationFlag)
     model_evaluation.evaluation(x=sig, y=mises, dy=dmisesdsig)
